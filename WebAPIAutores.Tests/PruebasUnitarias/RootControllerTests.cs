e2f2d35f4ce17eabb98436acf4e6595114b39e8e
Angelo Gonzalez <angelo.gonzalez@live.com>
2022-05-30 13:51:58 -0400

    Mocks para Suplantar Dependencias

---
+++ WebAPIAutores.Tests/PruebasUnitarias/RootControllerTests.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApiAutores.Controllers.V1;
+using WebAPIAutores.Tests.Mocks;
+
+namespace WebAPIAutores.Tests.PruebasUnitarias
+{
+    [TestClass]
+    public class RootControllerTests
+    {
+        [TestMethod]
+        public async Task SiUsuarioEsAdmin_Obtenemos4Links()
+        {
+            // Preparacion
+            var authorizationService = new AuthorizationServiceMock();
+            authorizationService.Resultado = AuthorizationResult.Success();
+            var rootController = new RootController(authorizationService);
+            rootController.Url = new URLHelperMock();
+
+            // Ejecucion
+            var resultado = await rootController.Get();
+
+            //Verificacion
+            Assert.AreEqual(4, resultado.Value.Count());
+        }
+
+        [TestMethod]
+        public async Task SiUsuarioNoEsAdmin_Obtenemos2Links()
+        {
+            // Preparacion
+            var authorizationService = new AuthorizationServiceMock();
+            authorizationService.Resultado = AuthorizationResult.Failed();
+            var rootController = new RootController(authorizationService);
+            rootController.Url = new URLHelperMock();
+
+            // Ejecucion
+            var resultado = await rootController.Get();
+
+            //Verificacion
+            Assert.AreEqual(2, resultado.Value.Count());
+        }
+    }
+}
